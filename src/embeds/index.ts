b222d00e2645b6e164d29148afa3b5ca07c7c0a7
NaijunWang <wnj123e@gmail.com>
2026-08-08 03:26:53 +0800

    feat: add bilibili video embedding function

--- src/embeds/index.ts
+++ src/embeds/index.ts
@@ -1,5 +1,6 @@
 import LinkCard from "./link-card/LinkCard.astro";
 import YouTube from "./youtube/YouTube.astro";
 import Excalidraw from "./excalidraw/Excalidraw.astro";
+import Bilibili from "./bilibili/Bilibili.astro";
 
-export { LinkCard, YouTube, Excalidraw };
+export { LinkCard, YouTube, Excalidraw, Bilibili };
